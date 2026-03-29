02425afc2bcf6a6574d0a6c9dc2b5a1c81b0738f
nur240495-prog <nur240495@gmail.com>
2026-03-29 15:43:47 +0300

    server.js

--- server.js
+++ server.js
@@ -7,17 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-// SAVE CODE
+// 👉 Басты бет
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// 👉 Publish сақтау
 app.post("/save", (req, res) => {
-  const code = req.body.code;
-
-  fs.writeFileSync(path.join(__dirname, "public/site.html"), code);
-
+  fs.writeFileSync(
+    path.join(__dirname, "public/site.html"),
+    req.body.code
+  );
   res.send("Saved!");
 });
 
-// START SERVER
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running...");
 });
