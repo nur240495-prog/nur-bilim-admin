e17d82615d94fa6d6a991d663ad44bdbc17b164a
nur240495-prog <nur240495@gmail.com>
2026-03-29 15:56:49 +0300

    server.js

--- server.js
+++ server.js
@@ -7,21 +7,30 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-// 👉 Басты бет
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+const PASSWORD = "Nur1995"; // 🔐 пароль
+
+// LOGIN тексеру
+app.post("/login", (req, res) => {
+  if (req.body.password === PASSWORD) {
+    res.send({ success: true });
+  } else {
+    res.send({ success: false });
+  }
 });
 
-// 👉 Publish сақтау
+// EDITOR беті
+app.get("/editor", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/editor.html"));
+});
+
+// САЙТ САҚТАУ
 app.post("/save", (req, res) => {
   fs.writeFileSync(
     path.join(__dirname, "public/site.html"),
     req.body.code
   );
-  res.send("Saved!");
+  res.send({ url: "/site.html" });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running...");
-});
+app.listen(PORT, () => console.log("Server running"));
